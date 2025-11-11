5b75b6898e7d45cf6eb4aa5b03f41f8b19531cc4
Abhishek <Abhishekraut8697@gmail.com>
2025-11-12 00:30:39 +0530

    added otp error class

---
+++ src/utils/otpError.ts
@@ -0,0 +1,20 @@
+import { ApiError } from "./apiError.js";
+
+export type OTPCode =
+    | 'EXPIRED'
+    | 'INVALID'
+    | 'TOO_MANY_ATTEMPTS'
+    | 'RATE_LIMIT_REACHED';
+
+class OtpError extends ApiError {
+    public code: OTPCode;
+
+    constructor(code: OTPCode) {
+        const status = code === 'INVALID' || code === 'EXPIRED' ? 401 : 429;
+        super(status, code, [], '');
+        Object.setPrototypeOf(this, new.target.prototype);
+        this.name = 'OTPError';
+        this.code = code;
+    }
+
+}
