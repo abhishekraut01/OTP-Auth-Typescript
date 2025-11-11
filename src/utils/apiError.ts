1eeaab1f22ebccaa23bde8ff1885d00a53473236
Abhishek <Abhishekraut8697@gmail.com>
2025-11-11 23:22:43 +0530

    added apiError uitility

---
+++ src/utils/apiError.ts
@@ -0,0 +1,29 @@
+class ApiError extends Error {
+    public statusCode: number
+    public success: boolean
+    public errors: any[]
+    public isOperational: boolean
+
+    constructor(
+        statusCode: number = 500,
+        message: string = "Internal server Error",
+        errors: any[] = [],
+        stack: string = ''
+    ) {
+        super(message)
+        Object.setPrototypeOf(this, new.target.prototype);
+
+
+        this.statusCode = statusCode
+        this.errors = errors
+        this.success = false
+        this.isOperational = false
+
+
+        if (stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
