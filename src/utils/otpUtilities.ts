d3cb5b0ba1a4cf0a0fcf9206d65e304e21a307f2
Abhishek <Abhishekraut8697@gmail.com>
2025-11-12 18:44:48 +0530

    utility:added otp verify fn

--- src/utils/otpUtilities.ts
+++ src/utils/otpUtilities.ts
@@ -1,6 +1,6 @@
 import crypto from 'crypto'
 
-const handleGenerateOtp = (): string => {
+export const handleGenerateOtp = (): string => {
     const OTP_LENGTH = 6
     const min = 10 ** (OTP_LENGTH - 1)
     const max = 10 ** OTP_LENGTH - 1
@@ -9,7 +9,7 @@ const handleGenerateOtp = (): string => {
     return String(OTP)
 }
 
-const handleHashOTP = async (otp: string, salt?: string) => {
+export const handleHashOTP = async (otp: string, salt?: string) => {
     const s = salt ?? crypto.randomBytes(16).toString('hex');
 
     const hash = await new Promise<string>((resolve, reject) => {
@@ -25,3 +25,18 @@ const handleHashOTP = async (otp: string, salt?: string) => {
     }
 }
 
+export async function handleVerifyOTP(otp: string, hash: string, salt: string) {
+    const testHash = await new Promise<string>((resolve, reject) => {
+        crypto.pbkdf2(otp, salt, 100_000, 64, 'sha512', (err, derivedKey) => {
+            if (err) return reject(err);
+            resolve(derivedKey.toString('hex'));
+        });
+    });
+
+    // Use timing-safe comparison
+    const valid = crypto.timingSafeEqual(
+        Buffer.from(testHash, 'hex'),
+        Buffer.from(hash, 'hex')
+    );
+    return valid;
+}
