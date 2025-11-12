d13b728140cbd19bfb09e4beedaf930edc1cf693
Abhishek <Abhishekraut8697@gmail.com>
2025-11-12 15:21:11 +0530

    adding async handler

--- src/utils/otpError.ts
+++ src/utils/otpError.ts
@@ -6,12 +6,12 @@ export type OTPCode =
     | 'TOO_MANY_ATTEMPTS'
     | 'RATE_LIMIT_REACHED';
 
-class OtpError extends ApiError {
+export class OtpError extends ApiError {
     public code: OTPCode;
 
     constructor(code: OTPCode) {
         const status = code === 'INVALID' || code === 'EXPIRED' ? 401 : 429;
-        super(status, code, [], '');
+        super(status, code, [], '')
         Object.setPrototypeOf(this, new.target.prototype);
         this.name = 'OTPError';
         this.code = code;
