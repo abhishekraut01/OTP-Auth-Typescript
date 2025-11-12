2d88f0f7051ee6bee075565aee9a6398290840dc
Abhishek <Abhishekraut8697@gmail.com>
2025-11-12 18:35:48 +0530

    added handleHashUtility

--- src/utils/otpUtilities.ts
+++ src/utils/otpUtilities.ts
@@ -8,3 +8,20 @@ const handleGenerateOtp = (): string => {
     const OTP = crypto.randomInt(min, max + 1)
     return String(OTP)
 }
+
+const handleHashOTP = async (otp: string, salt?: string) => {
+    const s = salt ?? crypto.randomBytes(16).toString('hex');
+
+    const hash = await new Promise<string>((resolve, reject) => {
+        crypto.pbkdf2(otp, s, 100_000, 64, 'sha512', (err, derivedKey) => {
+            if (err) return reject(err);
+            resolve(derivedKey.toString('hex'));
+        });
+    });
+
+    return {
+        hash,
+        salt: s
+    }
+}
+
